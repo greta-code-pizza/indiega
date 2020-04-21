db8e23fbf63f2fbf37fcf743a9749fd4583eb402
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-04-21 19:19:31 +0200

    plop

--- router.php
+++ router.php
@@ -2,4 +2,10 @@
 
 if($_SERVER['REQUEST_URI'] == '/admin') {
   require_once('administration/index.php');
+} else {
+  if($_GET['page'] == "plop") {
+    echo('Page Plop !');
+  } else {
+    echo('404 not found');
+  }
 }
